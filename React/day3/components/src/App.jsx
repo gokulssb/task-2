3b794fb059826c32956399371f6d32cd97f75836
gokul <gokulparamesh6@gmail.com>
2026-07-06 19:55:02 +0530

    update

---
+++ React/day3/components/src/App.jsx
@@ -0,0 +1,19 @@
+import Task from "./Component/Task"
+import "./App.css"
+const App=()=>{
+ return(
+  <>
+
+   <div>
+    <h1>This is component</h1>
+    <img src="./favicon.svg" alt="" />
+   </div>
+   <Task/>
+
+
+  </>
+ )
+
+ 
+}
+export default App
---
+++ React/day3/components/src/Component/Task.jsx
@@ -0,0 +1,18 @@
+import pic from "../assets/hero.png"
+const Task=()=>{
+ return(
+  <>
+
+   <div className="lorem">
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, beatae? Modi voluptate labore odit architecto veritatis et rem, nostrum eius. Officia doloremque, veniam magnam repellendus blanditiis rem natus laboriosam quam.</h1>
+   
+   </div>
+    <img src={pic} alt="" />
+
+
+  </>
+ )
+
+
+}
+export default Task
